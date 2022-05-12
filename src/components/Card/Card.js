0356b82eb8e4084eaf4c4369f7c2cb2fb716b4ee
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2022-05-12 22:20:14 +0800

    meal page nemev

---
+++ src/components/Card/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  CardContainer,
+  ContainerImage,
+  ContainerContent,
+  Content,
+  ButtonContainer
+} from "./CardElements";
+const Card = (props) => {
+  return (
+    <CardContainer>
+      <ContainerImage>
+        <img src={props.task.image_url} />
+      </ContainerImage>
+      <ContainerContent>
+        <Content>
+          <h3>{props.task.title}</h3>
+          <ul>
+            <li>
+              <span>Publisher :</span> {props.task.publisher}
+            </li>
+            <li>
+              <span>Recipe :</span> {props.task.recipe_id}
+            </li>
+            <li>
+              <span>Social Rank :</span> {props.task.social_rank.toFixed(0)}
+            </li>
+          </ul>
+        </Content>
+        <ButtonContainer>
+          <a href={props.task.source_url} target="_blank">
+            Read more
+          </a>
+        </ButtonContainer>
+      </ContainerContent>
+    </CardContainer>
+  );
+};
+
+export default Card;
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -11,21 +11,86 @@ import {
   Form,
   Icon
 } from "./SignInElements";
+import { ErrorContainer } from "../Error";
+import useInput from "../../hooks/loginhook";
+import { useHistory } from "react-router-dom";
 
 const SignIn = () => {
+  const history = useHistory();
+  const {
+    value: enteredPassword,
+    isValid: enteredPasswordIsValid,
+    hasError: passwordInputHasError,
+    valueChangeHandler: passwordChangedHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPasswordInput
+  } = useInput((value) => value.trim().length >= 6);
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (enteredPasswordIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    // console.log(enteredEmailIsValid);
+
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
+    resetPasswordInput();
+    resetEmailInput();
+    history.push("/");
+  };
   return (
     <>
       <Container>
         <FormWrap>
           <Icon to="/">Omnifood</Icon>
           <FormContent>
-            <Form action="#">
+            <Form onSubmit={formSubmissionHandler}>
               <FormH1>Sign in to your account </FormH1>
               <FormLabel htmlFor="for">Email</FormLabel>
-              <FormInput type="email" required />
+              <FormInput
+                type="email"
+                required
+                onChange={emailChangeHandler}
+                onBlur={emailBlurHandler}
+                value={enteredEmail}
+              />
+              {emailInputHasError && (
+                <ErrorContainer>Please enter a valid email.</ErrorContainer>
+              )}
+
               <FormLabel htmlFor="for">Password</FormLabel>
-              <FormInput type="password" required />
-              <FormButton type="submit">Continue</FormButton>
+              <FormInput
+                type="password"
+                minlength={6}
+                required
+                onChange={passwordChangedHandler}
+                onBlur={passwordBlurHandler}
+                value={enteredPassword}
+              />
+              {passwordInputHasError && (
+                <ErrorContainer>
+                  Password must not be wrong. Password must be more than 6
+                  characters
+                </ErrorContainer>
+              )}
+
+              <FormButton type="submit">Submit</FormButton>
               <Text>Forget password</Text>
             </Form>
           </FormContent>
